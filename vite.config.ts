1b86ca77970464d886f2576934bf1ebf850311ef
mj0x0 <psycoil88@gmail.com>
2026-05-08 14:50:05 +0300

    feat: add theme watcher to Vite config for live reload on theme.css changes

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,18 @@ import { defineConfig } from "vite"
 
 export default defineConfig({
   base: "./",
-  plugins: [react()],
+  plugins: [
+    react(),
+    {
+      name: "watch-theme",
+      configureServer(server) {
+        server.watcher.add("public/theme.css")
+        server.watcher.on("change", path => {
+          if (path.endsWith("public/theme.css")) {
+            server.ws.send({ type: "full-reload" })
+          }
+        })
+      },
+    },
+  ],
 })
